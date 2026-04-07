d98f044764014c72244262ee9b74a490545a7513
Rafi <rafi@unitdtechnologies.com>
2026-04-07 13:22:55 +0530

    first commit

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,18 +1,27 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "https://akceramicworldadmin.unitdtechnologies.com:4001"
+// });
+
+// // 🔥 attach token
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+// export default api;
+
+// import axios from 'axios'
+
 const api = axios.create({
-  baseURL: "http://localhost:5000"
+baseURL: 'https://akceramicworldadmin.unitdtechnologies.com:4000',
+// baseURL: 'http://localhost:3001'
 });
 
-// 🔥 attach token
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-export default api;
+export default api
